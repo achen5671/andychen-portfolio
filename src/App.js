9dd33f49a897c7961f0547b444f04f2ea78e0ae6
Andy Chen <44120813+achen5671@users.noreply.github.com>
2022-06-28 10:21:01 -0400

    Header page with animation and social icons

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+// We are using react-icons, is fontawesome a better alternative?
+// These icons are not the same as the one in the design
+// TODO: Choose the right fonts
+import { FiGithub } from 'react-icons/fi';
+import {RiLinkedinLine, RiDiscordLine, RiInstagramLine, RiSpotifyLine} from 'react-icons/ri'
+
+function App() {
+  return (
+    <div className="App">
+      {/* TODO: Rename class name */}
+      <header className="App-header">
+        <div className='hide_overflow'>
+          <h3 className='name'>Andy Chen</h3>
+          <p className='desc'>Full Stack Developer & UI / UX Designer</p>
+        </div>
+
+        <div className='medias'>
+          <ul className='media-list'>
+            <li><FiGithub/></li>
+            <li><RiLinkedinLine/></li>
+            <li><RiDiscordLine/></li>
+            <li><RiInstagramLine/></li>
+            <li><RiSpotifyLine/></li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
