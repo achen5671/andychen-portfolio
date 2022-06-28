95ab5e0b537fad869092e2656c037f03ae9a6342
Andy Chen <44120813+achen5671@users.noreply.github.com>
2022-06-28 14:02:24 -0400

    Add about me text

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
   return (
     <div className="App">
       {/* TODO: Rename class name */}
+      {/* TODO: Move to it's own componenet that accepts a `title` and `desc` prop */}
+      {/* Header */}
       <header className="App-header">
         <div className='hide_overflow'>
           <h3 className='name'>Andy Chen</h3>
@@ -26,6 +28,11 @@ function App() {
           </ul>
         </div>
       </header>
+
+      <div className='about-me'>
+        <h1 className='right-parallax'>ABOUT ME</h1>
+        <p>Junior  Engineer with a passion for design and manifesting ideas into reality!</p>
+      </div>
     </div>
   );
 }
