70601dbe150cd8e7f99f5576c769265b878dc7f8
Andy Chen <44120813+achen5671@users.noreply.github.com>
2022-06-30 10:25:07 -0400

    Add skills icon. Add new js component for Skills section

---
+++ src/Skills.js
@@ -0,0 +1,41 @@
+// import './App.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
+export default function Skills() {
+    return (
+    <div>
+        <section>
+            <h1>Skills</h1>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <Image src='./skills_icon/react_icon.jpeg' className='skills_icon'/>
+                        <p className='icon_name'>ReactJS</p>
+                    </Col>
+                    <Col>
+                        <Image src='./skills_icon/python_icon.webp' className='skills_icon'/>
+                        <p className='icon_name'>Python</p>
+                    </Col>
+                    <Col>
+                        <Image src='./skills_icon/git_icon.png' className='skills_icon'/>
+                        <p className='icon_name'>Git</p>
+                    </Col>
+                    <Col>
+                        <Image src='./skills_icon/html5.png' className='skills_icon'/>
+                        <p className='icon_name'>HTML5</p>
+                    </Col>
+                    <Col>
+                        <Image src='./skills_icon/CSS_icon.png' className='skills_icon'/>
+                        <p className='icon_name'>CSS</p>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    </div>
+    )
+}
