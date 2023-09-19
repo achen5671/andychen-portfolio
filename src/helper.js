98cf76a64189ca29fd550cfe033a08bae67b5ad1
Andy Chen <44120813+achen5671@users.noreply.github.com>
2023-09-18 20:42:07 -0400

    add images

--- src/helper.js
+++ src/helper.js
@@ -1,31 +1,29 @@
 export const getToolImage = (type) => {
-
     switch (type) {
         case 'html':
             return 'https://achen5671.github.io/andychen-portfolio/images/tools/html5.png';
         case 'css':
             return 'https://achen5671.github.io/andychen-portfolio/images/admin-portal.png'
         case 'js':
-            return;
+            return 'https://achen5671.github.io/andychen-portfolio/images/javascript.svg';
         case 'ts':
-            console.log('nice')
             return 'https://achen5671.github.io/andychen-portfolio/images/typescript.png'
         case 'bootstrap':
-            return;
+            return 'https://achen5671.github.io/andychen-portfolio/images/bootstrap.png';
         case 'psql':
             return 'https://achen5671.github.io/andychen-portfolio/images/psql.svg';
         case 'node':
-            return;
+            return 'https://achen5671.github.io/andychen-portfolio/images/node.svg';
         case 'python':
-            return;
+            return 'https://achen5671.github.io/andychen-portfolio/images/python.png';
         case 'react':
-            return;
+            return 'https://achen5671.github.io/andychen-portfolio/images/react.png';
         case 'android-studio':
-            return;        
+            return 'https://achen5671.github.io/andychen-portfolio/images/android-studio.png';        
         case 'react-native':
-            return;        
+            return 'https://achen5671.github.io/andychen-portfolio/images/react.png';        
         case 'jira':
-            return;     
+            return 'https://achen5671.github.io/andychen-portfolio/images/jira.png';
         default:
             return '';   
     }
